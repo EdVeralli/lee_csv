27fe227c3d5c50386d1154e2006495382999e38d
Eduardo Damian Veralli <edveralli@gmail.com>
2022-08-21 21:48:06 -0300

    Added process Header´s output file. Problem in Append CSV

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,17 @@ use std::error::Error;
 use std::io;
 use std::process;
 use csv::ReaderBuilder;
-use csv::WriterBuilder;
+use csv::{QuoteStyle, WriterBuilder};
 use serde::Deserialize;
 use pad::{PadStr, Alignment};
+use std::fs::OpenOptions;
 
 
 #[derive(Debug, Deserialize)]
 struct RecordIn {
     legajo : String,
-    sector :Option<u64>,
-    cuilIn : String,
+    sector : String,
+    cuil : String,
     //#[serde(rename = "tipodoc")]
     doc_tipo: String,
     doc_nro : String,
@@ -27,25 +28,96 @@ struct RecordIn {
     compromiso :u32,
     saldo_tota :u32,
     //#[serde(rename = "salvenc")]
-    saldo_venc :u32,
+    //saldo_venc :Option<u64>,
+    saldo_venc : String,
     informacio : String,
 }
 
+#[derive(serde::Serialize)]
+struct HeaderOut {
+    HDR_CUIT: String,
+    HDR_TIPO_REG : String,
+    HDR_MATRICULA: String,
+    HDR_PROVINCIA: String,
+    HDR_GRADO: String,
+    HDR_RETORNO: String,
+    BLANCOS1: String,
+    HDR_ARCHIVO: String,
+    HDR_FECHA_GRABACION: String,
+    HDR_HORA_GRABACION: String,
+    BLANCOS2: String,
+}
+
+
 fn procesa_csv() -> Result<(), Box<dyn Error>> {
-  
+
+
+    let mut file = OpenOptions::new()
+    .write(true)
+    .append(true)
+    .open("test.csv")
+    .unwrap();
+    let mut wtr2 = csv::Writer::from_writer(file);
+    
+
+
+    let blanco01: String;
+    let blanco02: String;
+    let blancos01: String;
+    let blancos02: String;
+    
+    let blanco01 = " ";
+    let blancos01 = blanco01.pad_to_width(8);
+    let blanco02 = " ";
+    let blancos02 = blanco02.pad_to_width(250);
+
+    let newRecHeader = HeaderOut {
+        HDR_CUIT: "30606513433".to_string(),
+        HDR_TIPO_REG: "HH".to_string(),
+        HDR_MATRICULA: "      735".to_string(),
+        HDR_PROVINCIA: "C".to_string(),
+        HDR_GRADO: "MM".to_string(),
+        HDR_RETORNO: "  ".to_string(),
+        BLANCOS1: blancos01,
+        HDR_ARCHIVO: "M".to_string(),
+        HDR_FECHA_GRABACION: "AAAAMMDD".to_string(),
+        HDR_HORA_GRABACION: " HHMMSS".to_string(),
+        BLANCOS2: blancos02,
+    };
+        
+
+
     let mut rdr = csv::ReaderBuilder::new()
              .has_headers(true)
              .delimiter(b';')
              .double_quote(true)
              .escape(Some(b'\\'))
-             .flexible(true)
+             //.flexible(true)
              .comment(Some(b'#'))
              .from_path("Cuotas.csv")?;
 
-    let mut wtr = WriterBuilder::new()
-              .has_headers(true)
+    let mut wtr = csv::WriterBuilder::new()
+              .has_headers(false)
+              //.flexible(false)
+              //.quote_style(QuoteStyle::Never)
               .from_path("Salida_Cuotas.csv")
               .unwrap();
+              //.delimiter(b':');
+              //.flexible(false);
+             
+
+    let mut wtr02 = csv::WriterBuilder::new()
+        .has_headers(false)
+        .quote_style(QuoteStyle::Never)
+        .from_path("Salida_Cuotas.csv")
+        .unwrap();
+
+        //.flexible(false);
+        //.delimiter(b':');
+
+
+    let retHeader = wtr02.serialize(newRecHeader);
+    wtr02.flush()?;
 
     #[derive(serde::Serialize)]
     struct RecOut {
@@ -75,7 +147,7 @@ fn procesa_csv() -> Result<(), Box<dyn Error>> {
 
         let filler16:String;
         let filler16 = " ";
-        let espacios16 = filler16.pad_to_width_with_char(16, '-');
+        let espacios16 = filler16.pad_to_width_with_char(16, ' ');
 
         let filler208:String;
         let filler208 = " ";
@@ -84,7 +156,7 @@ fn procesa_csv() -> Result<(), Box<dyn Error>> {
 
         //println!("{:?}",record.cuilIn);
         let newRec = RecOut {
-            cuilOut: record.cuilIn.trim().to_string().pad_to_width(11),
+            cuilOut: record.cuil.trim().to_string().pad_to_width(11),
             docTipoOut: record.doc_tipo.trim().to_string().pad_to_width(3),
             docNroOut: record.doc_nro.trim().to_string().pad(20, '0', Alignment::Right, true),
             diasAtrasOut : record. dias_atras.trim().to_string().pad_to_width_with_char(3, '0'),
